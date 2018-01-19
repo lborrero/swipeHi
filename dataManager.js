b7ca9ae1c457a1135516eafab84e9c78ce4c2d0f
BHVR\lluz <miximum1@gmail.com>
2018-01-19 17:30:30 -0500

    Added fileWriting bundle

---
+++ dataManager.js
@@ -0,0 +1,28 @@
+var obj = {
+   table: []
+};
+
+obj.table.push({id: 1, square:2});
+
+var json = JSON.stringify(obj);
+
+var fs = require('fs');
+
+function apiName(){
+
+fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data); //now it an object
+    obj.table.push({id: 2, square:3}); //add some data
+    json = JSON.stringify(obj); //convert it back to json
+    fs.writeFile('myjsonfile.json', json, 'utf8', function(err){
+	if (err) throw err;
+		console.log('saved');
+	});
+}});
+
+};
+
+exports.apiName = apiName;
--- index.js
+++ index.js
@@ -6,6 +6,10 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
+var dataManager = require('./dataManager.js');
+
+dataManager.apiName();
+
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
