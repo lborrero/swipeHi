1e8c64806a0472ea6053bab6a149d53f8906ddfc
Leonardo <leonardo@Leonardos-MBP.lan>
2018-02-22 21:45:32 -0500

    added geolocation! Yay!

--- NativeApp/App.js
+++ NativeApp/App.js
@@ -14,16 +14,36 @@ export default class ButtonBasics extends Component {
   constructor(props){
     super(props);
     this.state = {
-      numberOfMessages: 10,
-      thisisTrue: true
+      numberOfMessages: 1,
+      latitude: null,
+      longitude: null,
+      error: null,
     }
+
+    this._onPressButton = this._onPressButton.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
   }
 
   _onPressButton() {
-    this.setState({
-      thisisTrue: false
-    });
-    Alert.alert('You tapped the button!' + this.state.numberOfMessages)
+    componentDidMount();
+    this.setState(prevState => ({
+      numberOfMessages: prevState.numberOfMessages+1
+    }));
+    Alert.alert('You tapped the button!');
   }
 
   oneMessage(words){
@@ -49,6 +69,11 @@ export default class ButtonBasics extends Component {
             onPress={this._onPressButton}
             title="Press Me"
           />
+        </View>
+        <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Latitude: {this.state.latitude}</Text>
+          <Text>Longitude: {this.state.longitude}</Text>
+          {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
         </View>
       </View>
     );
